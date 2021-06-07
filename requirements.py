8664c6a6b78b3af8e7f7b1f92e1f06e1b16eed6a
sangjunSeung <tkdwns6292@naver.com>
2021-06-07 12:44:51 +0900

    add file

---
+++ requirements.py
@@ -0,0 +1,5 @@
+import os
+os.system('pip install opencv-contrib-python && pip install opencv-python && pip install tensorflow')
+os.system('pip install scipy && pip install scikit-learn && pip install h5py')
+os.system('pip install matplotlib && pip install Pillow && pip install requests')
+os.system('pip install psutil && pip install imageio')
